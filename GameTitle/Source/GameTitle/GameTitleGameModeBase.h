684d13251097cc81f8976e79f7d1090245edec5e
Mizaak- <mikyriz99@gmail.com>
2020-11-19 10:16:59 +0100

    - First project commit + initial gitignore config

---
+++ GameTitle/Source/GameTitle/GameTitleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameTitleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMETITLE_API AGameTitleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
